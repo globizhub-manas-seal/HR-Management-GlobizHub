10aa2b38336b62016a90a741571639e1e559da48
manas105 <132210161+manas105@users.noreply.github.com>
2026-08-31 17:40:01 +0530

    remove sentry for now

--- hrms-backend-v1/src/main.ts
+++ hrms-backend-v1/src/main.ts
@@ -9,15 +9,17 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
-  // Initialize Sentry
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [nodeProfilingIntegration()],
-    // Tracing
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
-  });
+  // Initialize Sentry conditionally
+  if (process.env.SENTRY_DSN) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      integrations: [nodeProfilingIntegration()],
+      // Tracing
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set sampling rate for profiling - this is relative to tracesSampleRate
+      profilesSampleRate: 1.0,
+    });
+  }
 
   app.useLogger(app.get(Logger));
   app.use(helmet());
